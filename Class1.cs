fd066bf1c4ee42841f0d8c8403767ddcb8706d14
shuimu <3048627@qq.com>
2022-10-23 11:33:37 +0800

    添加项目文件。

---
+++ Class1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Pipes;
+using System.Security.Principal;
+using System.Text;
+using System.Threading;
+
+public class PipeClient
+{
+
+    private static int numClients = 1;
+
+    //创建管道，发送指令并返回值
+    public static string MainSend(string s)
+    {
+        var pipeClient =
+            new NamedPipeClientStream(".", "testpipe",
+                PipeDirection.InOut, PipeOptions.None,
+                TokenImpersonationLevel.Impersonation);
+
+        pipeClient.Connect();
+
+        var ss = new StreamString(pipeClient);
+        // Validate the server's signature string.
+        string receive = null;
+        if (ss.ReadString() == "I am the one true server!")
+        {
+
+            //客户端发送数据
+            ss.WriteString(s);
+
+            //客户端接受信息
+            receive = ss.ReadString();
+
+            // The client security token is sent with the first write.
+            // Send the name of the file whose contents are returned
+            // by the server.
+            //ss.WriteString("D:\\textfile.txt");
+        }
+        else
+        {
+            Console.WriteLine("Server could not be verified.");
+        }
+        pipeClient.Close();
+
+        return receive;
+    }
+
+
+
+}
+
+public class StreamString
+{
+    private Stream ioStream;
+    private UnicodeEncoding streamEncoding;
+
+    public StreamString(Stream ioStream)
+    {
+        this.ioStream = ioStream;
+        streamEncoding = new UnicodeEncoding();
+    }
+
+    public string ReadString()
+    {
+        int len;
+        len = ioStream.ReadByte() * 256;
+        len += ioStream.ReadByte();
+        var inBuffer = new byte[len];
+        ioStream.Read(inBuffer, 0, len);
+
+        return streamEncoding.GetString(inBuffer);
+    }
+
+    public int WriteString(string outString)
+    {
+        byte[] outBuffer = streamEncoding.GetBytes(outString);
+        int len = outBuffer.Length;
+        if (len > UInt16.MaxValue)
+        {
+            len = (int)UInt16.MaxValue;
+        }
+        ioStream.WriteByte((byte)(len / 256));
+        ioStream.WriteByte((byte)(len & 255));
+        ioStream.Write(outBuffer, 0, len);
+        ioStream.Flush();
+
+        return outBuffer.Length + 2;
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DBMS_UI
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.grid.MouseLeftButtonDown += (o, e) => { DragMove(); };
+        }
+
+        private void UnFold(object sender, RoutedEventArgs e)
+        {
+            if(Shuimu.IsExpanded == false)
+            {
+                Shuimu.IsExpanded = true;
+            }
+            else
+            {
+                Shuimu.IsExpanded = false;
+            }
+        }
+
+        private void test(object sender, RoutedEventArgs e)
+        {
+            string s  = PipeClient.MainSend("ShuiMu");
+            Button b1 = new Button();
+            WrapPanel wrapPanel1 = new WrapPanel();
+            //MessageBox.Show(s);
+            Expander e1 = new Expander();
+            Style Expanderstyle = this.FindResource("ExpanderStyle1")as Style;
+            HeaderedContentControl headeredContentControl2 = new HeaderedContentControl();
+            Image image = this.FindResource("Mysql") as Image;
+            TextBlock textBlock=new TextBlock();
+            textBlock.Text = s;
+            wrapPanel1.Children.Add(image);
+            wrapPanel1.Children.Add(textBlock);
+            b1.Style = this.FindResource("ButtonThree") as Style;
+            b1.Content = wrapPanel1;
+            b1.ContextMenu = this.FindResource("MySqlMenu") as ContextMenu;
+            headeredContentControl2.Content=b1;
+            e1.Name = s;
+            e1.Style = Expanderstyle;
+            e1.Header = headeredContentControl2;
+            MySqlMenu.Children.Add(e1);
+            //MySqlMenu.Children.Add(b1);
+            //MessageBox.Show("Hello");
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+        private void LinkMenu_Initialized(object sender, EventArgs e)
+
+        {
+
+            //设置右键菜单为null
+
+            this.Link.ContextMenu = null;
+
+        }
+        private void LinkMenu_Click(object sender, RoutedEventArgs e)
+
+        {
+
+            //目标
+
+            this.LinkMenu.PlacementTarget = this.Link;
+
+            //位置
+
+            this.LinkMenu.Placement = PlacementMode.Bottom;
+
+            //显示菜单
+
+            this.LinkMenu.IsOpen = true;
+
+        }
+        private void LinkWindow_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 LinkWindows = new Window1();
+            LinkWindows.Show();
+        }
+    }
+}
